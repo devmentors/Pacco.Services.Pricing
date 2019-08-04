d3acdf632d273910a2a31cb7cd77f99b50abe45d
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-08-04 10:03:53 +0200

    Logging, cleanup

--- src/Pacco.Services.Pricing.Api/Queries/Handlers/GetOrderPricingHandler.cs
+++ src/Pacco.Services.Pricing.Api/Queries/Handlers/GetOrderPricingHandler.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Convey.CQRS.Queries;
+using Microsoft.Extensions.Logging;
 using Pacco.Services.Pricing.Api.Core.Services;
 using Pacco.Services.Pricing.Api.DTO;
 using Pacco.Services.Pricing.Api.Exceptions;
@@ -11,11 +12,14 @@ namespace Pacco.Services.Pricing.Api.Queries.Handlers
     {
         private readonly ICustomersServiceClient _client;
         private readonly ICustomerDiscountsService _service;
+        private readonly ILogger<GetOrderPricingHandler> _logger;
 
-        public GetOrderPricingHandler(ICustomersServiceClient client, ICustomerDiscountsService service)
+        public GetOrderPricingHandler(ICustomersServiceClient client, ICustomerDiscountsService service,
+            ILogger<GetOrderPricingHandler> logger)
         {
             _client = client;
             _service = service;
+            _logger = logger;
         }
 
         public async Task<OrderPricingDto> HandleAsync(GetOrderPricing query)
@@ -29,6 +33,10 @@ namespace Pacco.Services.Pricing.Api.Queries.Handlers
 
             var customerDiscount = _service.CalculateDiscount(customer.AsEntity());
             var orderDiscountPrice = query.OrderPrice - (customerDiscount * query.OrderPrice);
+            
+            _logger.LogInformation($"Calculated the pricing for the customer with id: {query.CustomerId}, " +
+                                   $"base order price: {query.OrderPrice} $, discount: {customerDiscount} $, " +
+                                   $"final price: {orderDiscountPrice} $.");
 
             return new OrderPricingDto
             {
